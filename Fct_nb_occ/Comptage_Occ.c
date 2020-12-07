7979bd3a7afdaa0881af2c38e0148ec1089434a0
Romain <romain.eybrard@gmail.com>
2020-12-07 21:00:01 +0100

    Fonction pour compter les occurences (v 1.1)

--- Fct_nb_occ/Comptage_Occ.c
+++ Fct_nb_occ/Comptage_Occ.c
@@ -1,12 +1,7 @@
-//
-// Created by Romain on 30/11/2020.
-//
-
 #include "Comptage_Occ.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#define TEXT "input.txt"
 
 Huffman_Tree* allocate_huffman_branch(){
     Huffman_Tree *HT;
@@ -25,64 +20,41 @@ List_Huffman_Tree* allocate_list_huffman_branch(){
     return LHT;
 }
 
-int get_size(char *filename){
-    struct stat st;
-    int size;
-    stat(filename, &st);
-    size = st.st_size;
-    return size;
-}
-
-void printchar(char* chain, int size){
-    for (int i = 0; i < size; ++i) {
-        printf("%c",chain[i]);
-    }
-
-}
-char* fileread(char* chain) {
-
-    FILE *file;
-    int x = get_size("C:\\Users\\Romain\\CLionProjects\\Huffman_Coding\\Alice.txt");
-    chain = realloc(chain, x);
-    file = fopen("C:\\Users\\Romain\\CLionProjects\\Huffman_Coding\\Alice.txt", "a+");
-    if (file != NULL){
-        fscanf(file,"%[^\0]", chain);
-        fclose(file);
-    }
-    return chain;
-}
-List_Huffman_Tree *lettersearch( const char* chain,int size,int SizeHuff){
+List_Huffman_Tree *lettersearch(int SizeHuff){
     List_Huffman_Tree *nboccletters;
     char c ='\0';
+    FILE* file = fopen(TEXT,"r");
     nboccletters =allocate_list_huffman_branch();
-    nboccletters->element->struct_c.character = chain[0];
+    nboccletters->element->struct_c.character = fgetc(file);
     nboccletters->element->struct_c.occurence = 1;
     SizeHuff = 1;
-    for (int i = 1; i < size; ++i) {
-        c = chain[i];
-        List_Huffman_Tree *p =nboccletters;
-        int a=0;
-        while (p->next != NULL){
+    while (c != EOF){
+        c = fgetc(file);
+        if (c>0){
+            List_Huffman_Tree *p =nboccletters;
+            int a=0;
+            while (p->next != NULL){
+                if (c == p->element->struct_c.character) {
+                    p->element->struct_c.occurence += 1;
+                    a = 1;
+                }
+                p = p->next;
+            }
             if (c == p->element->struct_c.character) {
                 p->element->struct_c.occurence += 1;
                 a = 1;
             }
-            p = p->next;
-        }
-        if (c == p->element->struct_c.character) {
-            p->element->struct_c.occurence += 1;
-            a = 1;
-        }
-        if (a == 0) {
-            List_Huffman_Tree *new = allocate_list_huffman_branch();
-            new->element->struct_c.character = c;
-            new->element->struct_c.occurence = 1;
-            new->next = NULL;
-            p->next = new;
-            SizeHuff += 1;
+            if (a == 0) {
+                List_Huffman_Tree *new = allocate_list_huffman_branch();
+                new->element->struct_c.character = c;
+                new->element->struct_c.occurence = 1;
+                new->next = NULL;
+                p->next = new;
+                SizeHuff += 1;
 
 
 
+            }
         }
     }
     return nboccletters;
@@ -95,12 +67,9 @@ void printlist(List_Huffman_Tree *nboccletters){
     }
     printf("la lettre %c a %d occurences\n",p->element->struct_c.character, p->element->struct_c.occurence);
 }
-void Main_Occ(){
+List_Huffman_Tree * Main_Occ(){
     int SizeHuff = 0;
-    int size = get_size("C:\\Users\\Romain\\CLionProjects\\Huffman_Coding\\Alice.txt");
-    char *chain = NULL;
-    chain = fileread(chain);
-    List_Huffman_Tree *nboccletters  = lettersearch(chain,size,SizeHuff);
-    //printchar(chain,size);
+    List_Huffman_Tree *nboccletters  = lettersearch(SizeHuff);
     //printlist(nboccletters);
+    return nboccletters;
 }
