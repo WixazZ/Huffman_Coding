2ee879b06023c9b1e28f94f647fe7a1108c18fe3
Alexis <alexis.paradiso@efrei.net>
2020-12-08 23:21:40 +0100

    amélioration de l'efficacité des fonctions

--- Fct_nb_occ/Comptage_Occ.h
+++ Fct_nb_occ/Comptage_Occ.h
@@ -4,28 +4,32 @@
 
 #ifndef HUFFMAN_CODING_LOT_2_H
 #define HUFFMAN_CODING_LOT_2_H
+#include <stdlib.h>
+#include <stdio.h>
+
+#define INPUT "cmake-build-debug/files/input.txt"
+#define DICO "cmake-build-debug/files/dico.txt"
+#define OUTPUT "cmake-build-debug/files/output.txt"
+
 typedef struct occ_char{
     char character;
     int occurence;
 }Occ_Char;
 
 typedef struct huffman_tree{
-     Occ_Char struct_c;
+    Occ_Char struct_c;
     struct huffman_tree *right;
     struct huffman_tree *left;
 }Huffman_Tree;
 
-typedef struct list_huffman_tree {
+typedef struct list_huffman_node {
     Huffman_Tree *element;
-    struct list_huffman_tree *next;
+    struct list_huffman_node *next;
 
-}List_Huffman_Tree;
+}List_Huffman_Node;
 Huffman_Tree* allocate_huffman_branch();
-List_Huffman_Tree* allocate_list_huffman_branch();
-int get_size(char *filename);
-void printchar(char* chain, int size);
-char* fileread(char* chain);
-List_Huffman_Tree *lettersearch(const char* chain,int size,int SizeHuff);
-void printlist(List_Huffman_Tree *nboccletter);
-void Main_Occ();
+List_Huffman_Node* allocate_list_huffman_branch();
+List_Huffman_Node *lettersearch(int SizeHuff);
+void printlist(List_Huffman_Node *nboccletter);
+List_Huffman_Node * Main_Occ();
 #endif //HUFFMAN_CODING_LOT_2_H
