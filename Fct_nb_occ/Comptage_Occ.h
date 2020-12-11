459debc8ca3db450031a64302fba7f452e0281d4
Romain <romain.eybrard@gmail.com>
2020-12-11 22:25:04 +0100

    Ajout Doxygen

--- Fct_nb_occ/Comptage_Occ.h
+++ Fct_nb_occ/Comptage_Occ.h
@@ -1,6 +1,11 @@
-//
-// Created by Romain on 30/11/2020.
-//
+/*****************************************************************//**
+ * \file   Comptage_Occ.h
+ * \brief  Comptage_Occ header
+ *
+ * \author Romain EYBRARD (https://github.com/EzzaaX/)
+ * \version 1.0
+ * \date   December 2020
+ *********************************************************************/
 
 #ifndef HUFFMAN_CODING_LOT_2_H
 #define HUFFMAN_CODING_LOT_2_H
@@ -27,9 +32,31 @@ typedef struct list_huffman_node {
     struct list_huffman_node *next;
 
 }List_Huffman_Node;
+/**
+ * /brief function to allocate a new node
+ * /return HT Human node
+*/
 Huffman_Tree* allocate_huffman_branch();
+/**
+ * \brief function to add a node to the list
+ * \return LHT list of Huffman nodes
+*/
 List_Huffman_Node* allocate_list_huffman_branch();
+/**
+ * \brief Function creating the list of occurrences
+ * \param SizeHuff the current size of the list
+ * \return nboccletter list of occurrences
+*/
 List_Huffman_Node *lettersearch(int SizeHuff);
+/**
+ * \brief  list print for testing purpose
+ * \param nboccletter list of Huffman nodes
+*/
 void printlist(List_Huffman_Node *nboccletter);
+/**
+ * \brief Main function
+ * \param input.txt the input text
+ * \return nboccletter list of Huffman nodes
+*/
 List_Huffman_Node * Main_Occ();
 #endif //HUFFMAN_CODING_LOT_2_H
