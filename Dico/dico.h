9cdb7b0e766c9e029c32ad85877616700927387d
Arthur L <63108549+AL-Skaro@users.noreply.github.com>
2020-12-11 16:38:50 +0100

    Update dico.h
    Ajout docu doxygen

--- Dico/dico.h
+++ Dico/dico.h
@@ -1,6 +1,10 @@
-//
-// Created by Alexis Paradiso on 07/12/2020.
-//
+/*****************************************************************//**
+* \file dico.h
+* \brief Header of the dico
+*
+* \author Arthur Lin arthur.lin@efrei.net
+* \date December 2020
+*********************************************************************/
 
 #ifndef HUFFMAN_CODING_DICO_H
 #define HUFFMAN_CODING_DICO_H
